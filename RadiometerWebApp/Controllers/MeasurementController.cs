87d4b520bb109b9ee5b1d2a4784e203b72761c47
a.burgart02 <a.burgart02@gmail.com>
2024-01-05 23:14:13 +0500

    Add measurements upload and download

--- RadiometerWebApp/Controllers/MeasurementController.cs
+++ RadiometerWebApp/Controllers/MeasurementController.cs
@@ -1,11 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
+using RadiometerWebApp.Models;
 
 namespace RadiometerWebApp.Controllers;
 
 public class MeasurementController : Controller
 {
-    ApplicationContext _db;
-
+    private ApplicationContext _db;
+    
     public MeasurementController(ApplicationContext context)
     {
         _db = context;
@@ -15,13 +16,44 @@ public class MeasurementController : Controller
     [Route("measurement/{id}")]
     public IActionResult Index(int id)
     {
-        return View();
+        var measurement = _db.Measurements.ToList().FirstOrDefault(x => x.Id == id);
+        return View(measurement);
     }
     
     [HttpPost]
     [Route("upload-measurement")]
-    public void Index()
+    public void UploadMeasurement()
     {
+        var file = HttpContext.Request.Form.Files.GetFile("file");
         
+        using var fileStream = file.OpenReadStream();
+        var measurementFile = new byte[file.Length];
+        var number = fileStream.Read(measurementFile, 0, (int)file.Length);
+
+        var measurementData = HttpContext.Request.Form;
+        var measurement = new Measurement() { 
+            Surname = measurementData["surname"].ToString(), 
+            Name = measurementData["name"].ToString(), 
+            Patronymic = measurementData["patronymic"].ToString(),
+            Time = DateTime.Parse(measurementData["time"].ToString()).ToUniversalTime(),
+            Patient = measurementData["patient"].ToString(),
+            Device = measurementData["device"].ToString(),
+            Data = measurementFile,
+            Description = measurementData["description"].ToString()
+        };
+        
+        _db.Measurements.Add(measurement);
+        _db.SaveChanges();
+    }
+    
+    [HttpGet]
+    [Route("measurement/download/{id}")]
+    public IActionResult DownloadMeasurement(int id)
+    {
+        var data = _db.Measurements.ToList().FirstOrDefault(x => x.Id == id).Data;
+        var content = new MemoryStream(data);
+        var contentType = "application/octet-stream";
+        var fileName = "data";
+        return File(content, contentType, fileName);
     }
 }
--- RadiometerWebApp/Models/Measurement.cs
+++ RadiometerWebApp/Models/Measurement.cs
@@ -9,5 +9,17 @@ public class Measurement
 
     public int Id { get; set; }
 
-    public string Text { get; set; }
+    public string Surname { get; set; }
+    public string Name { get; set; }
+    public string Patronymic { get; set; }
+
+    public DateTime Time { get; set; }
+    
+    public string Patient { get; set; }
+    
+    public string Device { get; set; }
+
+    public byte[] Data { get; set; }
+
+    public string Description { get; set; }
 }
