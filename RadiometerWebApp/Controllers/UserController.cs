35070fea4da9a79cbcccd53cc4a9831efdc4641b
a.burgart02 <a.burgart02@gmail.com>
2024-02-14 18:50:55 +0500

    Add user update

--- RadiometerWebApp/Controllers/UserController.cs
+++ RadiometerWebApp/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 using System.Text.Json;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using RadiometerWebApp.Models;
 using RadiometerWebApp.Utils;
 
@@ -33,6 +34,59 @@ public class UserController : Controller
     }
     
     [Authorize(Roles = Role.Admin)]
+    [HttpPost]
+    [Route("delete-user")]
+    public IActionResult DeleteUser([FromBody] User user)
+    {
+        var dbUser = _db.Users.Include(x => x.Measurements)
+            .FirstOrDefault(x => x.Id == user.Id);
+        if (dbUser == null || dbUser.Measurements.Count > 0)
+            return BadRequest();
+
+        _db.Remove(dbUser);
+        _db.SaveChanges();
+        return Ok();
+    }
+    
+    [Authorize(Roles = Role.Admin)]
+    [HttpPut]
+    [Route("update-user")]
+    public IActionResult UpdateUser([FromBody] User user)
+    {
+        var dbUser = _db.Users.FirstOrDefault(x => x.Id == user.Id);
+        if (dbUser == null)
+            return BadRequest();
+
+        dbUser.Login = user.Login;
+        dbUser.Name = user.Name;
+        dbUser.Surname = user.Surname;
+        dbUser.Patronymic = user.Patronymic;
+        dbUser.BirthDate = user.BirthDate?.ToUniversalTime();
+        dbUser.Sex = user.Sex;
+        dbUser.Role = user.Role;
+        dbUser.Revoked = user.Revoked;
+        dbUser.Notes = user.Notes;
+        _db.SaveChanges();
+        return Ok();
+    }
+    
+    [Authorize(Roles = Role.Admin)]
+    [HttpPost]
+    [Route("update-user-password")]
+    public IActionResult DeleteToken([FromBody] User user)
+    {
+        var dbUser = _db.Users.FirstOrDefault(x => x.Id == user.Id);
+        if (dbUser == null)
+            return BadRequest();
+        
+        var salt = HashCalculator.GenerateRandomString();
+        dbUser.Salt = salt;
+        dbUser.Password = HashCalculator.CalculateHash(user.Password, salt);
+        _db.SaveChanges();
+        return Ok();
+    }
+    
+    [Authorize(Roles = Role.Admin)]
     [HttpGet]
     [Route("users")]
     public IActionResult GetUsers()
@@ -42,7 +96,7 @@ public class UserController : Controller
             Id = x.Id,
             Name = x.Name,
             Surname = x.Surname,
-            Patromymic = x.Patronymic,
+            Patronymic = x.Patronymic,
             BirthDate = x.BirthDate,
             Sex = x.Sex,
             Notes = x.Notes,
